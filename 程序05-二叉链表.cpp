4657c04b66dabcfac5f9d8aecf1f50a39a90d08e
Zhechen Lee <1586783310@qq.com>
2016-10-28 11:03:57 +0800

    Update 程序05-二叉链表.cpp

--- 程序05-二叉链表.cpp
+++ 程序05-二叉链表.cpp
@@ -4,139 +4,191 @@
 #endif
 using namespace std;
 
-#define showtime printf("time = %.15f\n", clock() /(double)CLOCKS_PER_SEC);
-// #define lson l, m, rt  <<  1
-// #define rson m + 1, r, rt  <<  1 | 1
-// #define root 1, n, 1
-#define lson T -> lchild
-#define rson T -> rchild
+#define showtime printf("time = %.15f\n", clock() / (double)CLOCKS_PER_SEC);
+// #define lson l, m, rt << 1
+// #define rson m + 1, r, rt << 1 | 1
+#define lson rt -> lchild
+#define rson rt -> rchild
+#define root 1, n, 1
 
-const int maxn = 205;
-const int maxm = 1e5 + 5;
-const int mod = 10000;
+const int maxn = 505;
+const int maxm = 1005;
+const int mod = 1e6 + 3;
 const double eps = 1e-8;
+const double pi = acos(-1.0);
+// const int inf = std::numeric_limits<int>::max();
 const int inf = 0x3f3f3f3f;
-const int MAX_N = 50005;
 
-typedef struct BiTNode
+struct BitNode
 {
-    char   data;       
-    struct BiTNode  *lchild;    
-    struct BiTNode  *rchild;    
-} BiTNode, *BiTree;
+	char data;
+	BitNode *lchild, *rchild;
 
-int build(BiTree &T, char *str, int &pos)
+	BitNode() {}
+	BitNode(char a, BitNode* l, BitNode* r)
+	{
+		data = a; lchild = l; rchild = r;
+	}
+};
+
+typedef BitNode *BitTree;
+
+bool build(BitTree &rt, char *str, int &pos)
 {
-    char ch = str[pos];
-    if(ch == 0)
-    {
-        T = 0;
-        return false;
-    }
+	char ch = str[pos];
+	if(ch == '0')
+	{
+		rt = 0;
+		return false;
+	}
 
-    pos++;
-    if(ch == '#') T = 0;
-    else
-    {
-        T = new BiTNode;
-        if(!T) return false;
+	pos++;
+	if(ch == '#') rt = 0;
+	else
+	{
+		rt = new BitNode;
+		if(!rt) return false;
+		rt -> data = ch;
 
-        T -> data = ch;
-        int l_state, r_state;
-        l_state = build(lson, str, pos);
-        r_state = build(rson, str, pos);
+		bool l_state = build(lson, str, pos);
+		bool r_state = build(rson, str, pos);
 
-        if(!l_state || !r_state) return false;
-    }
-
-    return true;
-}
-void PrintBiTree(BiTree &T)
-{
-    if(T)
-    {
-        printf("%c(", T -> data);
-
-        PrintBiTree(lson);
-        printf(", ");
-
-        PrintBiTree(rson);
-        printf(")");
-    }
-    else printf("#");
+		if(!l_state || !r_state) return false;
+	}
+	return true;
 }
 
-void DestroyBiTree(BiTree &T)
+void print(BitTree &rt)
 {
-    if(T)
-    {
-        DestroyBiTree(lson);
-        DestroyBiTree(rson);
-        delete T;
-        T = 0;
-    }
-}
-void PreOrder(BiTree T)
-{
-    if(T)
-    {
-        printf("%c", T -> data);
-        PreOrder(lson);
-        PreOrder(rson);
-    }
-}
-void InOrder(BiTree T)
-{
-    if(T)
-    {
-        InOrder(lson);
-        printf("%c", T -> data);
-        InOrder(rson);
-    }
-}
-void PostOrder(BiTree T)
-{
-    if(T)
-    {
-        PostOrder(lson);
-        PostOrder(rson);
-        printf("%c", T -> data);
-    }
-}
-void CountLeaf(BiTree T,  int &cnt)
-{
-    if(T)
-    {
-        if((!lson)&&(!rson))
-            cnt++;
-        CountLeaf(lson, cnt);
-        CountLeaf(rson, cnt);
-    }
-}
-int Depth(BiTree T)
-{
-    int dep(0);
-    if(!T) return 0;
-    else
-    {
-        int depthLeft = Depth(lson);
-        int depthRight = Depth(rson);
-        dep = 1 + max(depthLeft, depthRight);
-    }
+	if(rt)
+	{
+		printf("%c(", rt -> data);
 
-    return dep;
+		print(lson);
+		printf(", ");
+		print(rson);
+		printf(")");
+	}
+	else printf("#");
 }
-BiTree CopyTree(BiTree T)
-{
-    if(!T) return 0;
-    BiTree newT = new BiTNode;
-    if(!newT) return 0;
-    newT -> data = T -> data;
-    newT -> lchild = CopyTree(lson);
-    newT -> rchild = CopyTree(rson);
 
-    return newT;
+void destroy(BitTree &rt)
+{
+	if(rt)
+	{
+		destroy(lson);
+		destroy(rson);
+		delete rt;
+		rt = 0;
+	}
 }
+
+void pre_order(BitTree rt)
+{
+	if(rt)
+	{
+		printf("%c", rt -> data);
+		pre_order(lson);
+		pre_order(rson);
+	}
+}
+
+void in_order(BitTree rt)
+{
+	if(rt)
+	{
+		in_order(lson);
+		printf("%c", rt -> data);
+		in_order(rson);
+	}
+}
+
+void post_order(BitTree rt)
+{
+	if(rt)
+	{
+		post_order(lson);
+		post_order(rson);
+		printf("%c", rt -> data);
+	}
+}
+
+void count_leaf(BitTree rt, int &cnt)
+{
+	if(rt)
+	{
+		if(!lson && !rson) cnt++;
+		count_leaf(lson, cnt);
+		count_leaf(rson, cnt);
+	}
+}
+
+int get_depth(BitTree rt)
+{
+	int ret(0);
+	if(!rt) return 0;
+	else
+	{
+		int left_depth = get_depth(lson);
+		int right_depth = get_depth(rson);
+		ret = 1 + max(left_depth, right_depth);
+	}
+	return ret;
+}
+
+BitTree copy(BitTree rt)
+{
+	if(!rt) return 0;
+	BitTree new_root = new BitNode;
+	if(!new_root) return 0;
+
+	new_root -> data = rt -> data;
+	new_root -> lchild = copy(lson);
+	new_root -> rchild = copy(rson);
+
+	return new_root;
+}
+
+void solve()
+{
+	BitTree T;
+    char buf[maxn];
+    int pos(0), cnt(0);
+
+    printf("输入二叉树，以\'#\'表示空树，如\"A#B##\"表示\"A(#,B(#,#))\"：\n");
+    gets(buf);
+
+    build(T, buf, pos);
+    print(T);
+    printf("\n");
+
+    count_leaf(T, cnt);
+    printf("叶子节点个数：%d\n", cnt);
+
+    int depth = get_depth(T);
+    printf("二叉树的深度：%d\n", depth);
+
+    BitTree newT = copy(T);
+    printf("复制的二叉树：");
+    print(newT);
+    printf("\n");
+
+    printf("先序遍历：");
+    pre_order(T);
+    printf("\n");
+
+    printf("中序遍历：");
+    in_order(T);
+    printf("\n");
+
+    printf("后序遍历：");
+    post_order(T);
+    printf("\n");
+
+    destroy(T);
+    destroy(newT);
+}
+
 
 int main()
 {
@@ -145,42 +197,7 @@ int main()
     freopen("out.txt", "w", stdout);
 #endif
 
-    BiTree T;
-    char buf[maxn];
-    int pos(0), cnt(0);
-
-    printf("输入二叉树，以\'#\'表示空树，如\"A#B##\"表示\"A(#,B(#,#))\"：\n");
-    gets(buf);
-
-    build(T, buf, pos);
-    PrintBiTree(T);
-    printf("\n");
-
-    CountLeaf(T, cnt);
-    printf("叶子节点个数：%d\n", cnt);
-
-    int depth = Depth(T);
-    printf("二叉树的深度：%d\n", depth);
-
-    BiTree newT = CopyTree(T);
-    printf("复制的二叉树：");
-    PrintBiTree(newT);
-    printf("\n");
-
-    printf("先序遍历：");
-    PreOrder(T);
-    printf("\n");
-
-    printf("中序遍历：");
-    InOrder(T);
-    printf("\n");
-
-    printf("后序遍历：");
-    PostOrder(T);
-    printf("\n");
-
-    DestroyBiTree(T);
-    DestroyBiTree(newT);
+    solve();
 
 #ifndef ONLINE_JUDGE
     fclose(stdin);
@@ -190,4 +207,3 @@ int main()
 
     return 0;
 }
-
