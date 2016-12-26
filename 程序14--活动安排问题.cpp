5456537c3b4d310478123c159e680b4d8889e327
Zhechen Lee <1586783310@qq.com>
2016-12-26 21:38:14 +0800

    Update 程序14--活动安排问题.cpp

--- 程序14--活动安排问题.cpp
+++ 程序14--活动安排问题.cpp
@@ -1,39 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Sample Input
+/*
+11
+1   4
+3   5
+0   6
+5   7
+3   8
+5   9
+6   10
+8   11
+8   12
+2   13
+12  14
+*/
+
 const int maxn = 1005;
 int a[maxn], b[maxn];
 
 void solve()
 {
-    int n, cnt, tmp;
-    while(scanf("%d", &n))
+    int n, cnt;
+    scanf("%d", &n);
+    cnt = 1;
+    for (int i = 0; i < n; i++)
+        scanf("%d%d", &a[i], &b[i]);
+    for (int i = 0; i < n; i++)
     {
-    	if(n == 0) break;
-        cnt = 1;
-        for (int i = 0; i < n; i++)
-        	scanf("%d%d", &a[i], &b[i]);
-        for (int i = 0; i < n; i++)
+        for (int j = i; j < n; j++)
         {
-            for (int j = i; j < n; j++)
+            if (b[j] < b[i])
             {
-                if (b[j] < b[i])
-                {
-                	swap(a[i], a[j]);
-                	swap(b[i], b[j]);
-                }
+                swap(a[i], a[j]);
+                swap(b[i], b[j]);
             }
         }
-        int k(0);
-        for (int i = 1; i < n; i++)
-            if (a[i] >= b[k])  { cnt++; k = i; }
-        printf("%d\n", cnt);
     }
+    int k(0);
+    for (int i = 1; i < n; i++)
+        if (a[i] >= b[k])
+        {
+            cnt++;
+            k = i;
+            printf("选择时间段为 (%2d, %2d) 的活动\n", a[i], b[i]);
+        }
+    printf("选择活动总数为 %d\n", cnt);
 }
 
 int main()
 {
-	solve();
+    freopen("a.txt", "r", stdin);
 
-	return 0;
+    solve();
+
+    return 0;
 }
